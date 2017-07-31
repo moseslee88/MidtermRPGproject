7b11dbadf3bec2777e3f2261fec2436dc7394626
Moses Lee <moseslee88@gmail.com>
2017-07-31 17:30:32 -0600

    created Stage Entity and Quest Entity

--- MidtermRPGproject/src/main/java/data/Quest.java
+++ MidtermRPGproject/src/main/java/data/Quest.java
@@ -1,5 +1,132 @@
 package data;
 
-public class Quest {
+import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Quest {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@Column(name = "name")
+	private String name;
+
+	@Column(name="description")
+	private String description;
+	
+	@Column(name="intro")
+	private String intro;
+	
+	@Column(name="conclusion")
+	private String conclusion;
+	
+	@Column(name="level_min")
+	private Integer levelMin;
+	
+	@Column(name="level_max")
+	private Integer levelMax;
+	
+	@Column(name="completed")
+	private Boolean completed;
+	
+	 @ManyToMany(mappedBy="questList")
+	 private List<Stage> stages; 
+	 
+	 @ManyToMany(mappedBy="quests")
+	 private List<Player> players;    
+	 
+	//getters and setters
+
+		public int getId() {
+			return id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getDescription() {
+			return description;
+		}
+
+		public void setDescription(String description) {
+			this.description = description;
+		}
+
+		public String getIntro() {
+			return intro;
+		}
+
+		public void setIntro(String intro) {
+			this.intro = intro;
+		}
+
+		public String getConclusion() {
+			return conclusion;
+		}
+
+		public void setConclusion(String conclusion) {
+			this.conclusion = conclusion;
+		}
+
+		public Integer getLevelMin() {
+			return levelMin;
+		}
+
+		public void setLevelMin(Integer levelMin) {
+			this.levelMin = levelMin;
+		}
+
+		public Integer getLevelMax() {
+			return levelMax;
+		}
+
+		public void setLevelMax(Integer levelMax) {
+			this.levelMax = levelMax;
+		}
+
+		public Boolean getCompleted() {
+			return completed;
+		}
+
+		public void setCompleted(Boolean completed) {
+			this.completed = completed;
+		}
+
+		public List<Stage> getStages() {
+			return stages;
+		}
+
+		public void setStages(List<Stage> stages) {
+			this.stages = stages;
+		}
+
+		public List<Player> getPlayers() {
+			return players;
+		}
+
+		public void setPlayers(List<Player> players) {
+			this.players = players;
+		}
+
+		@Override
+		public String toString() {
+			return "Quest [id=" + id + ", name=" + name + ", description=" + description + ", intro=" + intro
+					+ ", conclusion=" + conclusion + ", levelMin=" + levelMin + ", levelMax=" + levelMax
+					+ ", completed=" + this.getCompleted() + ", stages size=" + this.getStages().size() + "]";
+		}
+		
+		
+	 
 }
--- MidtermRPGproject/src/main/java/data/Stage.java
+++ MidtermRPGproject/src/main/java/data/Stage.java
@@ -1,5 +1,141 @@
 package data;
 
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+
+
+@Entity
 public class Stage {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column(name = "name")
+	private String name;
+
+	@Column(name="level")
+	private Integer level;
+	
+    @Column(name="intro")
+    private String intro;
+    
+    @Column(name="conclusion")
+    private String conclusion;
+    
+    @Column(name="choice")
+    private String choice;
+    
+    @ManyToOne
+    @JoinColumn(name="character_id")
+    private Character character;
+    
+    @Column(name="completed")
+    private Boolean completed;
+	 
+	// @OneToMany(mappedBy="customer")
+	// private List<Rental> renta;  
+    
+	 
+	 @ManyToMany
+	 @JoinTable(name="quest_stage",
+	 joinColumns= @JoinColumn(name="stage_id"),
+	 inverseJoinColumns=@JoinColumn(name="quest_id")
+	 )
+private List<Quest> questList;
+
+//getters and setters
+
+	public int getId() {
+		return id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getLevel() {
+		return level;
+	}
+
+	public void setLevel(int level) {
+		this.level = level;
+	}
+
+	public String getIntro() {
+		return intro;
+	}
+
+	public void setIntro(String intro) {
+		this.intro = intro;
+	}
+
+	public String getConclusion() {
+		return conclusion;
+	}
+
+	public void setConclusion(String conclusion) {
+		this.conclusion = conclusion;
+	}
+
+	public String getChoice() {
+		return choice;
+	}
+
+	public void setChoice(String choice) {
+		this.choice = choice;
+	}
+
+
+	public Character getCharacter() {
+		return character;
+	}
+
+
+	public void setCharacter(Character character) {
+		this.character = character;
+	}
+
+
+	public boolean isCompleted() {
+		return completed;
+	}
+
+	public void setCompleted(boolean completed) {
+		this.completed = completed;
+	}
+
+	public List<Quest> getQuests() {
+		return questList;
+	}
+
+	public void setQuests(List<Quest> quests) {
+		this.questList = quests;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Stage [id=" + id + ", name=" + name + ", level=" + level + ", intro=" + intro + ", conclusion="
+				+ conclusion + ", choice=" + choice + ", character=" + this.getCharacter() + ", completed=" + this.isCompleted()
+				+ "]";
+	}
+	 
+
+
+
 
 }
