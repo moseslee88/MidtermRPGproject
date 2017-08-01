3b39a2f61f43d0086024d52202dc827993cb2e89
Moses Lee <moseslee88@gmail.com>
2017-08-01 15:14:54 -0600

    updated ShopTest to test player and game_character associations

--- RPGJPAProject/test/test/ShopTest.java
+++ RPGJPAProject/test/test/ShopTest.java
@@ -43,19 +43,36 @@ public class ShopTest {
     
     @Test
     public void test_game_character_association (){
-    	GameCharacter gc = em.find(GameCharacter.class, 5);
+    	GameCharacter gc = em.find(GameCharacter.class, 5);  //test for id#5 in GameCharacter class
     	assertNotNull(gc);
-    	assertEquals("Banshee", gc.getName());
+    	assertEquals("Bogle", gc.getName());   //the fifth name in the database is Bogle, not Banshee
+    	assertEquals(36, gc.getExperienceGiven());     //should be 36
     	
     }
     @Test
     public void test_player_gamecharacter_mappings() {
-        Player p =em.find(Player.class, 1);
+        Player p =em.find(Player.class, 1);   //'admin' in ENUM TypeOfUser
          List<GameCharacter> chs = p.getGameCharacters();
         assertNotNull(p);
-        assertEquals("user@user.com", p.getEmail());
-        assertEquals(3, chs.size());  //tests list of game characters for player 1
+        assertEquals("admin@admin.com", p.getEmail());
+        assertEquals(55, chs.size());  //tests list of game characters for player 1
         //assertEquals(p.getPhone(), “3333333”);
     }
+    
+    @Test
+    public void test_player_friend_association (){
+    	Player p =em.find(Player.class, 2);   //'player' in ENUM TypeOfUser
+    	
+    }
+    
+    @Test
+    public void test_player_quest_association (){
+    	GameCharacter g
+    }
+    
+    @Test
+    public void test_player_usertype_association (){
+    	GameCharacter g
+    } 
 	
 }
