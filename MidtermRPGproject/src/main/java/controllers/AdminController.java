92bf962d7cab290413bbbd3c0dce54bd98b5f1dc
csgibson4 <csgibson4@gmail.com>
2017-08-01 17:12:47 -0600

    ...

--- MidtermRPGproject/src/main/java/controllers/AdminController.java
+++ MidtermRPGproject/src/main/java/controllers/AdminController.java
@@ -1,17 +1,33 @@
 package controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import javax.crypto.*;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import data.AdminDao;
+import data.GameCharacter;
 
 @Controller
 public class AdminController {
 	
 	
 	@Autowired
-	AdminDao dao;
+	private AdminDao dao;
+	
+	@RequestMapping(path = "GetGameCharacters.do", method = RequestMethod.GET)
+	public String show(Model model) {
 
+		List<GameCharacter> characters = dao.indexGameCharacters();
+
+		model.addAttribute("characters", characters);
+
+		return "WEB-INF/views/admin/adminCharacter.jsp";
+	}
+
+	
 
 }
