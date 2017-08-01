5c39c98bf5e404c87d63b6b0cc0c80079242a925
csgibson4 <csgibson4@gmail.com>
2017-08-01 17:07:36 -0600

    admin dao and controller started

--- MidtermRPGproject/src/main/java/controllers/AdminController.java
+++ MidtermRPGproject/src/main/java/controllers/AdminController.java
@@ -1,8 +1,17 @@
 package controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import javax.crypto.*;
+
+import data.AdminDao;
 
 @Controller
 public class AdminController {
+	
+	
+	@Autowired
+	AdminDao dao;
+
 
 }
--- MidtermRPGproject/src/main/java/data/AdminDao.java
+++ MidtermRPGproject/src/main/java/data/AdminDao.java
@@ -1,8 +1,33 @@
 package data;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
+
 
 @Repository
 public interface AdminDao {
+	public List<GameCharacter> indexGameCharacters();
+	public GameCharacter showGameCharacter(int id);
+	public GameCharacter createGameCharacter(GameCharacter GameCharacter);
+	public GameCharacter updateGameCharacter(int id, GameCharacter GameCharacter);
+	public boolean destroyGameCharacter(int id);
+	
+	public List<Item> indexItems();
+	public Item showItem(int id);
+	public Item createItem(Item Item);
+	public Item updateItem(int id, Item Item);
+	public boolean destroyItem(int id);
 
+	public List<Player> indexPlayers();
+	public Player showPlayer(int id);
+	public Player createPlayer(Player Player);
+	public Player updatePlayer(int id, Player Player);
+	public boolean destroyPlayer(int id);
+	
+	public List<Quest> indexQuests();
+	public Quest showQuest(int id);
+	public Quest createQuest(Quest Quest);
+	public Quest updateQuest(int id, Quest Quest);
+	public boolean destroyQuest(int id);
 }
