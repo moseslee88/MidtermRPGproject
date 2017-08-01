33ad76ec2710bb44a4de443710a6878171886819
Moses Lee <moseslee88@gmail.com>
2017-08-01 13:46:49 -0600

    added ONE test

--- RPGJPAProject/test/test/ShopTest.java
+++ RPGJPAProject/test/test/ShopTest.java
@@ -1,6 +1,9 @@
 package test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -10,13 +13,13 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-
+import data.GameCharacter;
 import data.Player;
-import data.Shop;
 
 public class ShopTest {
 	private EntityManagerFactory emf = null;
 	private EntityManager em = null;
+	GameCharacter ch = null;
 
 	@Before
 	public void setup() throws Exception {
@@ -38,10 +41,12 @@ public class ShopTest {
         assertEquals(pass, true);
     }
     @Test
-    public void test_player_character_mappings() {
+    public void test_player_gamecharacter_mappings() {
         Player p =em.find(Player.class, 1);
+         List<GameCharacter> chs = p.getGameCharacters();
         assertNotNull(p);
         assertEquals(p.getEmail(),"user@user.com");
+        assertEquals(chs.size(), 3);  //tests list of game characters for player 1
         //assertEquals(p.getPhone(), “3333333”);
     }
 	
