e096250568be47cc7c27cbe3fc9440563ffb366b
broberson7801 <broberson7801@gmail.com>
2017-08-01 09:05:34 -0600

    correct pathing for tests updated

---
+++ MidtermRPGproject/test/test/ShopTest.java
@@ -0,0 +1,42 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import data.Inventory;
+import data.Shop;
+
+public class ShopTest {
+	private EntityManagerFactory emf = null;
+	private EntityManager em = null;
+
+	@Before
+	public void setup() throws Exception {
+		emf = Persistence.createEntityManagerFactory("MidtermRPGproject");
+		em = emf.createEntityManager();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		em.close();
+		emf.close();
+
+	}
+	
+	@Test
+	public void test_shop_for_inventory_id() {
+		Shop shop = em.find(Shop.class, 1);
+		Inventory inventory = shop.getInventory();
+		assertEquals("dog", inventory.getShops());
+	}
+	
+}
